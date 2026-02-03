ddd0801bd440b32ccfd2635e1e15d20ed312118e
No22-Broke <silvilius@gmail.com>
2026-02-03 09:47:26 +0200

    Bandoma ar susikuriau savo branch ar commitinu i maina

---
+++ lab8/task/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$a = 'Vilius';
+$b = 5;
+$c = 15.3;
+$d = true;
+$e = [59, 59.8, 60.6];
+$f($vardas = 'Vilius',
+    $amzius = 'Taip, yra',
+    $lytis = 'Pasirinktinai, Vyras');
+$g = null;
+print_r($a, $b, $c, $d, $e, $f, $g)
+?>
+    
+</body>
+</html>
+
+
+
