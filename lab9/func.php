8d2768c0e524b0c1d7ace18b28306ff68ab95ad7
Rol-Ka <rolandaskakta1@gmail.com>
2026-02-03 10:09:20 +0200

    Lab9 uzduotis

--- lab9/func.php
+++ lab9/func.php
@@ -1,1 +1,20 @@
 <?php
+echo "<pre>";
+
+$dydis = rand(100, 150);
+echo "1. Masyvo dydis: $dydis\n\n";
+
+$masyvas = [];
+
+for ($i = 0; $i < $dydis; $i++) {
+    $masyvas[] = rand(1, 100);
+}
+
+echo "2. Sugeneruotas masyvas:\n";
+print_r($masyvas);
+echo "\n";
+
+$suma = array_sum($masyvas);
+echo "3. Visų masyvo elementų suma: $suma\n";
+
+echo "</pre>";
