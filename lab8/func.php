7be431cad4bbef83f55a4e2456bddc54ff2ad41c
Tautvis-Dev <tautvkr@gmail.com>
2026-02-03 10:46:07 +0200

    lab8-baigtas

--- lab8/func.php
+++ lab8/func.php
@@ -1,1 +1,25 @@
 <?php
+
+$stringas = 'Stringas';
+var_dump($stringas);
+echo '<br/><hr/><br/>';
+
+$int = 59;
+var_dump($int);
+echo '<br/><hr/><br/>';
+
+$float = 59.8;
+var_dump($float);
+echo '<br/><hr/><br/>';
+
+$bool = true;
+var_dump($bool);
+echo '<br/><hr/><br/>';
+
+$array = [59, 59.8];
+var_dump($array);
+echo '<br/><hr/><br/>';
+
+$object["name"] = 'Name';
+$object["age"] = 31;
+var_dump($object);
